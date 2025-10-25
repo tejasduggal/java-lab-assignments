da5dcac5f583f977190105b96efe1e4b5599f718
tejasduggal <tejasduggal2006@gmail.com>
2025-10-25 11:32:00 +0530

    labs

--- lab6/shapechoice.java
+++ lab6/shapechoice.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class shapes {
+public class shapechoice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
